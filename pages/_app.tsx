c1f31ca2daf12a72d44dbb0a75cdad3f33ba47c1
AT010303 <101562874+AT010303@users.noreply.github.com>
2024-01-06 20:34:42 +0530

    fixed some linting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
-import 'styles/globals.scss'
-import type { AppProps } from 'next/app'
+import 'styles/globals.scss';
+import type { AppProps } from 'next/app';
+import type { ReactElement } from 'react';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }: AppProps): ReactElement {
+  return <Component {...pageProps} />;
 }
