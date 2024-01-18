245bfcde319042c79fd7d13678c79c66b70c4a38
AT010303 <101562874+AT010303@users.noreply.github.com>
2024-01-18 21:37:15 +0530

    moved global style in to theme

--- components/pages/StyledApp.tsx
+++ components/pages/StyledApp.tsx
@@ -5,16 +5,14 @@ import GlobalStyle from 'styles/GlobalStyle';
 import themes from 'styles/themes';
 
 const StyledApp: FC<{ children?: any }> = ({ children }) => (
-  <>
-    <GlobalStyle />
-    <SessionConsumer>
-      {({ themeName }) => (
-        <ThemeProvider theme={themes[themeName] || themes.defaultTheme}>
-          {children}
-        </ThemeProvider>
-      )}
-    </SessionConsumer>
-  </>
+  <SessionConsumer>
+    {({ themeName }) => (
+      <ThemeProvider theme={themes[themeName] || themes.defaultTheme}>
+        <GlobalStyle />
+        {children}
+      </ThemeProvider>
+    )}
+  </SessionConsumer>
 );
 
 export default StyledApp;
