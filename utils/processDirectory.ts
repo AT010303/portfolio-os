3e62057bd194a82528d7f43f063c28e26f77f20a
AT010303 <101562874+AT010303@users.noreply.github.com>
2024-01-22 19:50:48 +0530

    no startup processes

--- utils/processDirectory.ts
+++ utils/processDirectory.ts
@@ -1,24 +1,11 @@
 import dynamic from 'next/dynamic';
-import type { Processes } from 'types/contexts/process';
-import Taskbar from 'components/system/Taskbar/Taskbar';
+import type { Process } from 'types/contexts/process';
 
-const STARTUP_PROCESSES: string[] = ['Taskbar'];
-
-export const processDirectory: Processes = {
-  HelloWorld: {
-    Component: dynamic(() => import('components/apps/HelloWorld')),
-    hasWindow: true
-  },
-  Taskbar: {
-    Component: Taskbar
-  }
+export const HelloWorld: Process = {
+  Component: dynamic(() => import('components/apps/HelloWorld')),
+  hasWindow: true
 };
 
-export const getStartupProcesses = (): Processes =>
-  STARTUP_PROCESSES.reduce(
-    (processes, processId) => ({
-      ...processes,
-      [processId]: processDirectory[processId]
-    }),
-    {}
-  );
+export const Taskbar: Process = {
+  Component: dynamic(() => import('components/system/Taskbar/Taskbar'))
+};
