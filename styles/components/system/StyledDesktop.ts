33a916f1198035d25615f3592ff10609f6521a60
AT010303 <101562874+AT010303@users.noreply.github.com>
2024-01-25 20:58:25 +0530

    everything absolute

--- styles/components/system/StyledDesktop.ts
+++ styles/components/system/StyledDesktop.ts
@@ -3,11 +3,12 @@ import styled from 'styled-components';
 const StyledDesktop = styled.main`
   height: 100vh;
   width: 100vw;
-  position: fixed;
+  position: absolute;
   top: 0;
   left: 0;
   bottom: 0;
   right: 0;
+  overflow: hidden;
   background-color: ${({ theme }) => theme.colors.backgroundcolor};
 `;
 
