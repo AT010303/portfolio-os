8142d34d7e3eb1d33699f3433a423c62e60002f3
AT010303 <101562874+AT010303@users.noreply.github.com>
2024-01-25 22:34:47 +0530

    some more refactoring

--- components/system/Processes/ProcessLoader.tsx
+++ components/system/Processes/ProcessLoader.tsx
@@ -1,14 +1,15 @@
 import { ProcessConsumer } from 'contexts/process';
+import type { Process } from 'types/contexts/process';
 
 import RenderProcess from './RenderProcess';
 
+const ProcessesReducer = ([id, process]: [string, Process]) => (
+  <RenderProcess key={id} {...process} />
+);
+
 const ProcessLoader: React.FC = () => (
   <ProcessConsumer>
-    {({ processes }) =>
-      Object.entries(processes).map(([id, process]) => (
-        <RenderProcess key={id} {...process} />
-      ))
-    }
+    {({ processes }) => Object.entries(processes).map(ProcessesReducer)}
   </ProcessConsumer>
 );
 
