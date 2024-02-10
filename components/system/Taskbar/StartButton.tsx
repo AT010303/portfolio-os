ecfe3140529f5b48abbba8927d3c1e6151909d53
AT010303 <101562874+AT010303@users.noreply.github.com>
2024-02-10 20:16:59 +0530

    added start button click event to say hello in console for test

--- components/system/Taskbar/StartButton.tsx
+++ components/system/Taskbar/StartButton.tsx
@@ -1,10 +1,11 @@
+/* eslint-disable no-console */
 /* eslint-disable import/no-extraneous-dependencies */
 import { faWindows } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import StyledStartButton from 'styles/components/system/Taskbar/StyledStartButton';
 
 const StartButton = (): JSX.Element => (
-  <StyledStartButton title="START">
+  <StyledStartButton title="START" onClick={()=>{console.log("hello");}}>
     <FontAwesomeIcon icon={faWindows} />
   </StyledStartButton>
 );
